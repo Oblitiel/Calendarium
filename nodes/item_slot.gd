22f0286842b3629e7674f07b165e25a183308c9b
Oblitiel <oblitiel@gmail.com>
2026-02-06 16:35:00 +0100

    Rework Item Slot

--- nodes/item_slot.gd
+++ nodes/item_slot.gd
@@ -1,17 +1,21 @@
 class_name ItemSlot
 extends Area2D
 
-var item_placed : GrabbableItem
+var item_in_spot : GrabbableItem
 
+func _ready() -> void:
+	connect("area_exited",_on_area_exit)
 
 func _physics_process(_delta: float) -> void:
-	var items_in_spot : Array = get_overlapping_areas()
-	if (
-		items_in_spot.size() > 0
-		and not item_placed
-		and not items_in_spot[0].is_grabbed
-	):
-		item_placed = items_in_spot[0]
-		item_placed.global_position = global_position
-	else:
-		item_placed = null
+	pass
+	
+	if has_overlapping_areas() and (not item_in_spot ) and not get_overlapping_areas()[0].is_grabbed:
+		item_in_spot = get_overlapping_areas()[0]
+		var item_tween = item_in_spot.create_tween()
+		item_tween.tween_property(item_in_spot, "global_position", global_position, 0.5)
+		await item_tween.finished
+		item_tween.kill()
+
+func _on_area_exit(area : Area2D):
+	if area == item_in_spot:
+		item_in_spot = null
